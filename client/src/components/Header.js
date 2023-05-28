4a0a2a37f90f0cd1b95cec22f035ab07e9bcde99
Piyush Mathpal <mathpal37@gmail.com>
2023-05-29 00:01:14 +0530

    Message

---
+++ client/src/components/Header.js
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { UserContext } from "../UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_URI}/profile`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        'x-access-token': localStorage.token
+      }
+    })
+      .then((response) => response.json())
+      .then((userInfo) => {
+        setUserInfo(userInfo);
+      }).catch(err => {
+        console.log(err)
+        setUserInfo(null)
+      });
+  }, []);
+
+  function logout() {
+    fetch(`${process.env.REACT_APP_URI}/logout`, {
+      credentials: "include",
+      method: "POST",
+    });
+    localStorage.removeItem("token");
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
